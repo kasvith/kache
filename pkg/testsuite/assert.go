acac0020189923d7efb01e7e72ecb2e2351c023d
Kasun Vithanage <alankasun@gmail.com>
2018-08-04 18:18:52 +0530

    Updated Lists Added more unit tests

--- pkg/testsuite/assert.go
+++ pkg/testsuite/assert.go
@@ -1,31 +1,47 @@
 package testsuite
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 //AssertEqual Will do a typical assertation
-func AssertEqual(t *testing.T, expected interface{}, given interface{})  {
+func AssertEqual(t *testing.T, expected interface{}, given interface{}) {
 	if expected != given {
 		t.Errorf("Assertion failed, excepted %v : given %v", expected, given)
 	}
 }
 
-func AssertStringSliceEqual(t *testing.T, expected []string, given []string)  {
+func AssertStringSliceEqual(t *testing.T, expected []string, given []string) {
 	if !reflect.DeepEqual(expected, given) {
 		t.Errorf("Slice failed, excepted %v : given %v", expected, given)
 	}
 }
 
-func AssertNil(t *testing.T, i interface{})  {
+func AssertNil(t *testing.T, i interface{}) {
 	if !reflect.ValueOf(i).IsNil() {
 		t.Errorf("Given %v is not nil", i)
 	}
 }
 
-func ExceptError(t *testing.T, excepted error, given error)  {
+func ExceptError(t *testing.T, excepted error, given error) {
 	if excepted.Error() != given.Error() {
 		t.Errorf("Excepted %v, found %v", excepted, given)
+	}
+}
+
+func ContainsElements(t *testing.T, expected []string, given []string) {
+	for elem := range expected {
+		found := false
+		for test := range given {
+			if elem == test {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			t.Errorf("Element %v(%T) was not found in given array %v(%T)", elem, elem, given, given)
+		}
 	}
 }
