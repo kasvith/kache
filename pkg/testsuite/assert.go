1531146a4f9a5fcf577eb68491bca8721efd7318
Kasun Vithanage <alankasun@gmail.com>
2018-08-04 18:27:13 +0530

    Updated Testsuite to display types excepted

--- pkg/testsuite/assert.go
+++ pkg/testsuite/assert.go
@@ -8,25 +8,25 @@ import (
 //AssertEqual Will do a typical assertation
 func AssertEqual(t *testing.T, expected interface{}, given interface{}) {
 	if expected != given {
-		t.Errorf("Assertion failed, excepted %v : given %v", expected, given)
+		t.Errorf("Assertion failed, excepted [%T] %v : given [%T] %v", expected, expected, given, given)
 	}
 }
 
 func AssertStringSliceEqual(t *testing.T, expected []string, given []string) {
 	if !reflect.DeepEqual(expected, given) {
-		t.Errorf("Slice failed, excepted %v : given %v", expected, given)
+		t.Errorf("Slice failed, excepted [%T] %v : given [%T] %v", expected, expected, given, given)
 	}
 }
 
 func AssertNil(t *testing.T, i interface{}) {
 	if !reflect.ValueOf(i).IsNil() {
-		t.Errorf("Given %v is not nil", i)
+		t.Errorf("Given [%T] %v is not nil", i, i)
 	}
 }
 
 func ExceptError(t *testing.T, excepted error, given error) {
 	if excepted.Error() != given.Error() {
-		t.Errorf("Excepted %v, found %v", excepted, given)
+		t.Errorf("Excepted [%T] %v, found [%T] %v", excepted, excepted, given, given)
 	}
 }
 
