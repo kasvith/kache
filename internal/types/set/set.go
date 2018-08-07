298ac7442d93e09954dd5ad58e93fa06180b9477
Kasun Vithanage <alankasun@gmail.com>
2018-08-07 14:55:21 +0530

    Added Set to Types (#7)
    * Added set files
    
    * Set data structure added
    Unit tests are added
    
    * Changed order of the test cases
    
    * add vetting
    
    * Updated to have a pointer of mux

---
+++ internal/types/set/set.go
@@ -0,0 +1,232 @@
+package set
+
+import (
+	"sync"
+)
+
+type Set struct {
+	m   map[string]int
+	mux *sync.Mutex
+}
+
+func New() *Set {
+	return &Set{m: make(map[string]int), mux: &sync.Mutex{}}
+}
+
+func NewFromSlice(data []string) *Set {
+	m := make(map[string]int)
+	for _, value := range data {
+		m[value] = 1
+	}
+
+	return &Set{m: m, mux: &sync.Mutex{}}
+}
+
+func (set *Set) getMap() map[string]int {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+
+	m := make(map[string]int)
+
+	for key := range set.m {
+		m[key] = 1
+	}
+
+	return m
+}
+
+func (set *Set) Add(keys []string) int {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+
+	added := 0
+
+	for _, key := range keys {
+		if _, found := set.m[key]; !found {
+			set.m[key] = 1
+			added++
+		}
+	}
+
+	return added
+}
+
+// Card this will return number of elements in the set
+func (set *Set) Card() int {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+
+	return len(set.m)
+}
+
+func elems(m map[string]int) []string {
+	res := make([]string, len(m))
+	i := 0
+	for key := range m {
+		res[i] = key
+		i++
+	}
+
+	return res
+}
+
+func (set *Set) Elems() []string {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+
+	return elems(set.m)
+}
+
+func duplicateMap(m map[string]int) map[string]int {
+	dup := make(map[string]int)
+	for key, value := range m {
+		dup[key] = value
+	}
+
+	return dup
+}
+
+func (set *Set) Diff(sets []Set) []string {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+
+	dup := duplicateMap(set.m)
+
+	for i := 0; i < len(sets); i++ {
+		for _, key := range sets[i].Elems() {
+			delete(dup, key)
+		}
+	}
+
+	return elems(dup)
+}
+
+func (set *Set) DiffS(sets []Set) *Set {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+	dup := duplicateMap(set.m)
+
+	for i := 0; i < len(sets); i++ {
+		for _, key := range sets[i].Elems() {
+			delete(dup, key)
+		}
+	}
+
+	return &Set{m: dup, mux: &sync.Mutex{}}
+}
+
+func (set *Set) Exists(key string) int {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+
+	if _, found := set.m[key]; found {
+		return 1
+	}
+
+	return 0
+}
+
+func Intersection(sets []Set) []string {
+	minSetIdx := 0
+	for i := 0; i < len(sets); i++ {
+		if sets[minSetIdx].Card() > sets[i].Card() {
+			minSetIdx = i
+		}
+	}
+
+	minSet := &sets[minSetIdx]
+	sets = append(sets[:minSetIdx], sets[minSetIdx+1:]...)
+
+	// an empty set means empty intersection
+	if minSet.Card() == 0 {
+		return []string{}
+	}
+
+	// iterate through minimum set to find the intersection
+	results := make([]string, 0)
+	for _, v := range minSet.Elems() {
+		for i := 0; i < len(sets); i++ {
+			if sets[i].Exists(v) == 1 {
+				results = append(results, v)
+			}
+		}
+	}
+
+	return results
+}
+
+func IntersectionS(sets []Set) *Set {
+	return NewFromSlice(Intersection(sets))
+}
+
+func Move(key string, src, dest *Set) int {
+	src.mux.Lock()
+	defer src.mux.Unlock()
+	if _, found := src.m[key]; found {
+		delete(src.m, key)
+		dest.mux.Lock()
+		dest.m[key] = 1
+		dest.mux.Unlock()
+		return 1
+	}
+
+	return 0
+}
+
+func (set *Set) Delete(keys []string) int {
+	set.mux.Lock()
+	defer set.mux.Unlock()
+
+	deleted := 0
+
+	for _, key := range keys {
+		if _, ok := set.m[key]; ok {
+			delete(set.m, key)
+			deleted++
+		}
+	}
+
+	return deleted
+}
+
+func Union(sets []Set) []string {
+	maxSetIdx := 0
+	for i := 0; i < len(sets); i++ {
+		if sets[maxSetIdx].Card() < sets[i].Card() {
+			maxSetIdx = i
+		}
+	}
+
+	m := sets[maxSetIdx].getMap()
+	sets = append(sets[:maxSetIdx], sets[maxSetIdx+1:]...)
+
+	for i := 0; i < len(sets); i++ {
+		for _, v := range sets[i].Elems() {
+			m[v] = 1
+		}
+	}
+
+	return elems(m)
+}
+
+func UnionS(sets []Set) *Set {
+	maxSetIdx := 0
+	for i := 0; i < len(sets); i++ {
+		if sets[maxSetIdx].Card() < sets[i].Card() {
+			maxSetIdx = i
+		}
+	}
+
+	m := sets[maxSetIdx].getMap()
+	sets = append(sets[:maxSetIdx], sets[maxSetIdx+1:]...)
+
+	for i := 0; i < len(sets); i++ {
+		for _, v := range sets[i].Elems() {
+			m[v] = 1
+		}
+	}
+
+	return &Set{m: m, mux: &sync.Mutex{}}
+}
+
+// TODO implement pop and randomelement
---
+++ internal/types/set/set_test.go
@@ -0,0 +1,158 @@
+package set
+
+import (
+	"testing"
+
+	"github.com/kasvith/kache/pkg/testsuite"
+)
+
+func TestSet_Add(t *testing.T) {
+	set := New()
+
+	res := set.Add([]string{"hello", "world", "hello"})
+	testsuite.AssertEqual(t, 2, res)
+}
+
+func TestSet_Card(t *testing.T) {
+	set := New()
+
+	set.Add([]string{"hello", "world", "hello", "bye"})
+	testsuite.AssertEqual(t, 3, set.Card())
+}
+
+func TestSet_Elems(t *testing.T) {
+	set := New()
+
+	set.Add([]string{"hello", "world", "hello", "bye"})
+	res := set.Elems()
+
+	testsuite.ContainsElements(t, []string{"hello", "world", "bye"}, res)
+}
+
+func TestSet_Diff(t *testing.T) {
+	set1 := New()
+	set2 := New()
+	set3 := New()
+
+	set1.Add([]string{"a", "b", "c", "d"})
+	set2.Add([]string{"c"})
+	set3.Add([]string{"a", "b"})
+
+	el := set1.Diff([]Set{*set2, *set3})
+	testsuite.ContainsElements(t, []string{"d"}, el)
+}
+
+func TestSet_DiffS(t *testing.T) {
+	set1 := New()
+	set2 := New()
+	set3 := New()
+
+	set1.Add([]string{"a", "b", "c", "d"})
+	set2.Add([]string{"c"})
+	set3.Add([]string{"a", "b"})
+
+	el := set1.DiffS([]Set{*set2, *set3})
+	testsuite.ContainsElements(t, []string{"d"}, el.Elems())
+}
+
+func TestIntersection(t *testing.T) {
+	set1 := New()
+	set2 := New()
+	set3 := New()
+
+	set1.Add([]string{"a", "b", "c", "d"})
+	set2.Add([]string{"b"})
+	set3.Add([]string{"a", "b"})
+
+	inter := Intersection([]Set{*set1, *set2, *set3})
+	testsuite.ContainsElements(t, []string{"b"}, inter)
+
+	set4 := New()
+	inter = Intersection([]Set{*set1, *set2, *set3, *set4})
+	testsuite.ContainsElements(t, []string{}, inter)
+
+	set4.Add([]string{"x"})
+	inter = Intersection([]Set{*set1, *set2, *set3, *set4})
+	testsuite.ContainsElements(t, []string{}, inter)
+}
+
+func TestIntersectionS(t *testing.T) {
+	set1 := New()
+	set2 := New()
+	set3 := New()
+
+	set1.Add([]string{"a", "b", "c", "d"})
+	set2.Add([]string{"b"})
+	set3.Add([]string{"a", "b"})
+
+	inter := IntersectionS([]Set{*set1, *set2, *set3})
+	testsuite.ContainsElements(t, []string{"b"}, inter.Elems())
+
+	set4 := New()
+	inter = IntersectionS([]Set{*set1, *set2, *set3, *set4})
+	testsuite.ContainsElements(t, []string{}, inter.Elems())
+
+	set4.Add([]string{"x"})
+	inter = IntersectionS([]Set{*set1, *set2, *set3, *set4})
+	testsuite.ContainsElements(t, []string{}, inter.Elems())
+}
+
+func TestSet_Exists(t *testing.T) {
+	set := New()
+
+	set.Add([]string{"hello", "world", "hello", "bye"})
+	rep := set.Exists("hello")
+	testsuite.AssertEqual(t, 1, rep)
+	rep = set.Exists("nonexistent")
+	testsuite.AssertEqual(t, 0, rep)
+}
+
+func TestMove(t *testing.T) {
+	set1 := New()
+	set2 := New()
+
+	set1.Add([]string{"a", "b", "c", "d"})
+	set2.Add([]string{"b"})
+
+	res := Move("a", set1, set2)
+	testsuite.AssertEqual(t, 1, res)
+	testsuite.ContainsElements(t, []string{"b", "c", "d"}, set1.Elems())
+	testsuite.ContainsElements(t, []string{"a", "b"}, set2.Elems())
+
+	res = Move("unknown", set1, set2)
+	testsuite.AssertEqual(t, 0, res)
+}
+
+func TestSet_Delete(t *testing.T) {
+	set := New()
+
+	set.Add([]string{"hello", "world", "hello", "bye"})
+	rep := set.Delete([]string{"hello", "world", "nonexistent"})
+	testsuite.AssertEqual(t, 2, rep)
+}
+
+func TestUnion(t *testing.T) {
+	set1 := New()
+	set2 := New()
+	set3 := New()
+
+	set1.Add([]string{"b", "g"})
+	set2.Add([]string{"a", "b", "c", "d"})
+	set3.Add([]string{"a", "b", "f"})
+
+	union := Union([]Set{*set1, *set2, *set3})
+	testsuite.ContainsElements(t, []string{"a", "b", "c", "d", "g", "f"}, union)
+}
+
+func TestUnionS(t *testing.T) {
+	set1 := New()
+	set2 := New()
+	set3 := New()
+
+	set1.Add([]string{"b", "g"})
+	set2.Add([]string{"a", "b", "c", "d"})
+	set3.Add([]string{"a", "b", "f"})
+
+	union := UnionS([]Set{*set1, *set2, *set3})
+	testsuite.ContainsElements(t, []string{"a", "b", "c", "d", "g", "f"}, union.Elems())
+}
