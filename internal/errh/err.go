137ffc2f7ee58ba03ef8b20877f48e9980871f8a
Kasun Vithanage <alankasun@gmail.com>
2018-08-10 16:48:50 +0530

    error log n exit

--- internal/errh/err.go
+++ internal/errh/err.go
@@ -2,6 +2,7 @@ package errh
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -12,4 +13,18 @@ func PrintErrorAndExit(err error, exit int) {
 
 	fmt.Fprintln(os.Stderr, err)
 	os.Exit(exit)
+}
+
+func LogErrorAndExit(err error, exit int) {
+
+	if os.Getenv("ENV") == "DEBUG" {
+		panic(err)
+	}
+
+	log.Fatal(err)
+	os.Exit(exit)
+}
+
+func LogError(params ...string) {
+	log.Println(params)
 }
