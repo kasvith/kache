3f1882b0b5e5af98d249436d4265a6ebbe9bf1f6
Kasun Vithanage <alankasun@gmail.com>
2018-08-16 02:08:11 +0530

    string commands

--- internal/cmds/strs.go
+++ internal/cmds/strs.go
@@ -3,24 +3,29 @@ package cmds
 import (
 	"github.com/kasvith/kache/internal/db"
 	"github.com/kasvith/kache/internal/protcl"
+	"github.com/kasvith/kache/pkg/util"
 )
 
-func Get(d *db.DB, key string) protcl.Message {
-	val, err := d.Get(key)
+func Get(d *db.DB, args []string) protcl.Message {
+	if len(args) != 1 {
+		return protcl.Message{Rep: nil, Err: &protcl.ErrInsufficientArgs{Cmd: "get"}}
+	}
+
+	val, err := d.Get(args[0])
 	if err != nil {
-		return protcl.Message{Rep: nil, Err: err}
+		return protcl.Message{Rep: nil, Err: &protcl.ErrGeneric{Error: err}}
 	}
 
 	if val.Type != db.TypeString {
-		return protcl.Message{Rep: nil, Err: &protcl.ErrorWrongType{}}
+		return protcl.Message{Rep: nil, Err: &protcl.ErrWrongType{}}
 	}
 
-	return protcl.Message{Rep: &protcl., Err: nil}
+	return protcl.Message{Rep: protcl.NewBulkStringReply(false, util.ToString(val.Value)), Err: nil}
 }
 
 func Set(d *db.DB, args []string) protcl.Message {
-	if len(args) < 2 {
-		return protcl.Message{Rep: nil, Err: &protcl.ErrorInsufficientArgs{Cmd: "set"}}
+	if len(args) != 2 {
+		return protcl.Message{Rep: nil, Err: &protcl.ErrInsufficientArgs{Cmd: "set"}}
 	}
 
 	key := args[0]
@@ -28,11 +33,14 @@ func Set(d *db.DB, args []string) protcl.Message {
 
 	d.Set(key, db.NewDataNode(db.TypeString, -1, val))
 
-	return protcl.Message{Rep: "OK", Err: nil}
+	return protcl.Message{Rep: protcl.NewSimpleStringReply("OK"), Err: nil}
 }
 
-func Exists(d *db.DB, key string) protcl.Message {
-	found := d.Exists(key)
+func Exists(d *db.DB, args []string) protcl.Message {
+	if len(args) != 1 {
+		return protcl.Message{Rep: nil, Err: &protcl.ErrInsufficientArgs{Cmd: "get"}}
+	}
+	found := d.Exists(args[0])
 
-	return protcl.Message{Rep: found, Err: nil}
+	return protcl.Message{Rep: protcl.NewIntegerReply(found), Err: nil}
 }
