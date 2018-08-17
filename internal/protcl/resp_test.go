13e7dd9891a90343c83f7327e2ee44ea4a24bc5d
Kasun Vithanage <alankasun@gmail.com>
2018-08-17 18:38:49 +0530

    unittest for resp

---
+++ internal/protcl/resp_test.go
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c)  2018 Kasun Vithanage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package protcl
+
+import (
+	"github.com/kasvith/kache/pkg/testsuite"
+	"testing"
+)
+
+func TestIntegerReply_Reply(t *testing.T) {
+	rep := NewIntegerReply(1995)
+	testsuite.AssertEqual(t, ":1995\r\n", rep.Reply())
+}
+
+func TestNewSimpleStringReply(t *testing.T) {
+	rep := NewSimpleStringReply("foo")
+	testsuite.AssertEqual(t, "+foo\r\n", rep.Reply())
+}
+
+func TestBulkStringReply_Reply(t *testing.T) {
+	// test for nil strings
+	nilRep := NewBulkStringReply(true, "")
+	testsuite.AssertEqual(t, "$-1\r\n", nilRep.Reply())
+
+	// test for normal strings
+	rep := NewBulkStringReply(false, "bar")
+	testsuite.AssertEqual(t, "$3\r\nbar\r\n", rep.Reply())
+}
+
+func TestArrayReply_Reply(t *testing.T) {
+	replies := []Reply{NewBulkStringReply(false, "foo"), NewBulkStringReply(false, "foobar")}
+	arrRep := NewArrayReply(replies)
+
+	targetStr := "*2\r\n$3\r\nfoo\r\n$6\r\nfoobar\r\n"
+	testsuite.AssertEqual(t, targetStr, arrRep.Reply())
+}
