e884a87b927904052ba675f2d204643979345cba
Kasun Vithanage <alankasun@gmail.com>
2018-08-17 08:58:24 +0530

    application logs

--- internal/klogs/loggers.go
+++ internal/klogs/loggers.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/kasvith/kache/internal/config"
+	"github.com/kasvith/kache/internal/sys"
 	"github.com/sirupsen/logrus"
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -33,19 +35,37 @@ func InitLoggers(config config.AppConfig) {
 	}
 
 	if config.Logging && config.Logfile != "" {
-		fp, err := os.OpenFile(config.Logfile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+		// try to create folder path if not exists
+		err := sys.AutoCreateSubDirs(config.Logfile)
+
+		// if failed, we can skip for logging to a file, warn user and continue
 		if err != nil {
 			Logger.Warningf("%s cannot be opened, continue with stderr", config.Logfile)
-		} else {
-			multi := io.MultiWriter(os.Stderr, fp)
-			logrusLogger.Out = multi
+			return
 		}
+
+		// try to create the file
+		fp, err := os.OpenFile(config.Logfile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+
+		// if failed, skip and warn
+		if err != nil {
+			Logger.Warningf("%s cannot be opened, continue with stderr", config.Logfile)
+			return
+		}
+
+		// info about log file
+		path, err := filepath.Abs(config.Logfile)
+
+		if err != nil {
+			Logger.Errorf("cannot resolve absolute path for %s", config.Logfile)
+		} else {
+			Logger.Infof("log file is %s", path)
+		}
+
+		// use two writers
+		multi := io.MultiWriter(os.Stderr, fp)
+		logrusLogger.Out = multi
 	}
-
-	// logrusLogger will output to stderr by default
-	// logrusLogger will log the pid of the process
-
-	Logger.Info("OK")
 }
 
 type KacheFormatter struct {
@@ -53,7 +73,9 @@ type KacheFormatter struct {
 
 func (KacheFormatter) Format(e *logrus.Entry) ([]byte, error) {
 	buffer := bytes.Buffer{}
-	str := fmt.Sprintf("[%s] %s(%d): %s\n", strings.ToUpper(e.Level.String()[0:4]), e.Time.Format("2006-01-02 15:04:05"), e.Data["pid"], e.Message)
+	lvl := strings.ToUpper(e.Level.String()[0:4])
+	t := e.Time.Format("2006-01-02 15:04:05")
+	str := fmt.Sprintf("[%s] %s(%d): %s\n", lvl, t, e.Data["pid"], e.Message)
 	buffer.WriteString(str)
 
 	return buffer.Bytes(), nil
