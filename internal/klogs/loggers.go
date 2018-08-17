7e3d6dc5a291414e1775137a390965f38d67f5b5
Kasun Vithanage <alankasun@gmail.com>
2018-08-17 09:07:45 +0530

    unexport logger formatter

--- internal/klogs/loggers.go
+++ internal/klogs/loggers.go
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c)  2018 Kasun Vithanage
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package klogs
 
 import (
@@ -26,7 +50,7 @@ func InitLoggers(config config.AppConfig) {
 		logrusLogger.SetLevel(logrus.WarnLevel)
 	}
 
-	logrusLogger.Formatter = &KacheFormatter{}
+	logrusLogger.Formatter = &kacheFormatter{}
 	Logger = logrusLogger.WithFields(logrus.Fields{"pid": os.Getpid()})
 
 	// if we dont want logging, just discard all to a null device
@@ -68,10 +92,10 @@ func InitLoggers(config config.AppConfig) {
 	}
 }
 
-type KacheFormatter struct {
+type kacheFormatter struct {
 }
 
-func (KacheFormatter) Format(e *logrus.Entry) ([]byte, error) {
+func (kacheFormatter) Format(e *logrus.Entry) ([]byte, error) {
 	buffer := bytes.Buffer{}
 	lvl := strings.ToUpper(e.Level.String()[0:4])
 	t := e.Time.Format("2006-01-02 15:04:05")
