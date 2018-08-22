7015977fdaeab48bdaf138aed85e7c0a970f671e
Chyroc <chyroc@qq.com>
2018-08-23 07:48:03 +0800

    replace testsuite to testify part 5(hashmap) (#50)

--- pkg/types/hashmap/hmap_test.go
+++ pkg/types/hashmap/hmap_test.go
@@ -28,54 +28,56 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/kasvith/kache/pkg/testsuite"
+	testifyAssert "github.com/stretchr/testify/assert"
 )
 
 func TestHashMap_Set(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	rep := hm.Set("mykey", "myval")
-	testsuite.AssertEqual(t, 1, len(hm.m))
-	testsuite.AssertEqual(t, 1, rep)
+	assert.Len(hm.m, 1)
+	assert.Equal(1, rep)
 
 	rep = hm.Set("mykey", "updated")
-	testsuite.AssertEqual(t, 1, len(hm.m))
-	testsuite.AssertEqual(t, 0, rep)
+	assert.Len(hm.m, 1)
+	assert.Equal(0, rep)
 
 	rep = hm.Set("mykey1", "myval")
-	testsuite.AssertEqual(t, 2, len(hm.m))
-	testsuite.AssertEqual(t, 1, rep)
+	assert.Len(hm.m, 2)
+	assert.Equal(1, rep)
 }
 
 func TestHashMap_Get(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	hm.Set("mykey", "myval")
 
 	mykey := hm.Get("mykey")
-
-	testsuite.AssertEqual(t, "myval", mykey)
+	assert.Equal("myval", mykey)
 }
 
 func TestHashMap_GetNullKey(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	hm.Set("mykey", "myval")
 
 	mykey := hm.Get("mykey1")
-
-	testsuite.AssertEqual(t, "", mykey)
+	assert.Equal("", mykey)
 }
 
 func TestHashMap_KeysNullMap(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	keys := hm.Keys()
-
-	testsuite.AssertStringSliceEqual(t, []string{}, keys)
+	assert.Equal([]string{}, keys)
 }
 
 func TestHashMap_Keys(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	elements := make([]string, 10)
@@ -86,26 +88,28 @@ func TestHashMap_Keys(t *testing.T) {
 
 	keys := hm.Keys()
 
-	testsuite.AssertEqual(t, 10, len(keys))
-	testsuite.ContainsElements(t, elements, keys)
+	assert.Len(keys, 10)
+	assert.ElementsMatch(elements, keys)
 }
 
 func TestHashMap_Fields(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 	arr := make([]string, 20)
 
 	for i := 0; i < 10; i++ {
 		hm.Set("key"+strconv.Itoa(i), strconv.Itoa(i))
 		arr[i] = "key" + strconv.Itoa(i)
-		arr[i*2] = strconv.Itoa(i)
+		arr[i+10] = strconv.Itoa(i)
 	}
 
 	res := hm.Fields()
 
-	testsuite.ContainsElements(t, arr, res)
+	assert.ElementsMatch(arr, res)
 }
 
 func TestHashMap_Delete(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	hm.Set("key1", "val1")
@@ -114,128 +118,137 @@ func TestHashMap_Delete(t *testing.T) {
 
 	deleted := hm.Delete([]string{"key1"})
 
-	testsuite.AssertEqual(t, 1, deleted)
+	assert.Equal(1, deleted)
 
 	deleted = hm.Delete([]string{"key2", "key3"})
-	testsuite.AssertEqual(t, 2, deleted)
+	assert.Equal(2, deleted)
 
 	deleted = hm.Delete([]string{"nonexistent"})
-	testsuite.AssertEqual(t, 0, deleted)
+	assert.Equal(0, deleted)
 }
 
 func TestHashMap_Exists(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	hm.Set("key1", "val1")
 
 	key1Exists := hm.Exists("key1")
-	testsuite.AssertEqual(t, 1, key1Exists)
+	assert.Equal(1, key1Exists)
 
 	unknown := hm.Exists("nonexistent")
-	testsuite.AssertEqual(t, 0, unknown)
+	assert.Equal(0, unknown)
 }
 
 func TestHashMap_IncrementBy(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	// Test for non existent key
 	res, err := hm.IncrementBy("counter", 1)
-	testsuite.AssertEqual(t, nil, err)
-	testsuite.AssertEqual(t, 1, res)
+	assert.Equal(nil, err)
+	assert.Equal(1, res)
 
 	res, err = hm.IncrementBy("counter", 4)
-	testsuite.AssertEqual(t, nil, err)
-	testsuite.AssertEqual(t, 5, res)
+	assert.Equal(nil, err)
+	assert.Equal(5, res)
 
 	hm.Set("key", "val")
 	res, err = hm.IncrementBy("key", 1)
-	testsuite.AssertEqual(t, "invalid type, excepted integer", err.Error())
-	testsuite.AssertEqual(t, 0, res)
+	assert.Equal("invalid type, excepted integer", err.Error())
+	assert.Equal(0, res)
 }
 
 func TestHashMap_IncrementByFloat(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	// Test for non existent key
 	res, err := hm.IncrementByFloat("counter", 10)
-	testsuite.AssertEqual(t, nil, err)
-	testsuite.AssertEqual(t, float64(10), res)
+	assert.Equal(nil, err)
+	assert.Equal(float64(10), res)
 
 	fl, err := hm.IncrementByFloat("counter", 0.5)
-	testsuite.AssertEqual(t, nil, err)
-	testsuite.AssertEqual(t, 10.5, fl)
+	assert.Equal(nil, err)
+	assert.Equal(10.5, fl)
 
 	fl, err = hm.IncrementByFloat("counter", -5)
-	testsuite.AssertEqual(t, nil, err)
-	testsuite.AssertEqual(t, 5.5, fl)
+	assert.Equal(nil, err)
+	assert.Equal(5.5, fl)
 
 	hm.Set("key", "val")
 	fl, err = hm.IncrementByFloat("key", 1.5)
-	testsuite.AssertEqual(t, "invalid type, excepted float", err.Error())
-	testsuite.AssertEqual(t, float64(0), fl)
+	assert.Equal("invalid type, excepted float", err.Error())
+	assert.Equal(float64(0), fl)
 }
 
 func TestHashMap_Len(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
-	testsuite.AssertEqual(t, 0, hm.Len())
+	assert.Equal(0, hm.Len())
 	hm.Set("key", "val")
-	testsuite.AssertEqual(t, 1, hm.Len())
+	assert.Equal(1, hm.Len())
 	hm.Set("key2", "val")
-	testsuite.AssertEqual(t, 2, hm.Len())
+	assert.Equal(2, hm.Len())
 	hm.Delete([]string{"key2"})
-	testsuite.AssertEqual(t, 1, hm.Len())
+	assert.Equal(1, hm.Len())
 }
 
 func TestHashMap_SetBulk(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	res, err := hm.SetBulk([]string{"field1", "val1", "field2", "val2"})
-	testsuite.AssertEqual(t, nil, err)
-	testsuite.AssertEqual(t, "OK", res)
-	testsuite.AssertEqual(t, 2, hm.Len())
+	assert.Equal(nil, err)
+	assert.Equal("OK", res)
+	assert.Equal(2, hm.Len())
 
 	res, err = hm.SetBulk([]string{"field1", "val1", "field2"})
-	testsuite.AssertEqual(t, "invalid number of arguments", err.Error())
-	testsuite.AssertEqual(t, "", res)
+	assert.Equal("invalid number of arguments", err.Error())
+	assert.Equal("", res)
 }
 
 func TestHashMap_GetBulk(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	hm.Set("key1", "val1")
 	hm.Set("key2", "val2")
 
 	res := hm.GetBulk([]string{"key1", "nonexistent", "key2"})
-	testsuite.AssertStringSliceEqual(t, []string{"val1", "", "val2"}, res)
+	assert.Equal([]string{"val1", "", "val2"}, res)
 }
 
 func TestHashMap_Setx(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	rep := hm.Setx("key1", "val1")
 	val := hm.Get("key1")
-	testsuite.AssertEqual(t, 1, rep)
-	testsuite.AssertEqual(t, "val1", val)
+	assert.Equal(1, rep)
+	assert.Equal("val1", val)
 
 	rep = hm.Setx("key1", "val2")
 	val = hm.Get("key1")
-	testsuite.AssertEqual(t, 0, rep)
-	testsuite.AssertEqual(t, "val1", val)
+	assert.Equal(0, rep)
+	assert.Equal("val1", val)
 }
 
 func TestHashMap_FLen(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	hm.Set("key1", "val1")
 	n := hm.FLen("key1")
-	testsuite.AssertEqual(t, 4, n)
+	assert.Equal(4, n)
 
 	n = hm.FLen("nonexistent")
-	testsuite.AssertEqual(t, 0, n)
+	assert.Equal(0, n)
 }
 
 func TestHashMap_Vals(t *testing.T) {
+	assert := testifyAssert.New(t)
 	hm := New()
 
 	elements := make([]string, 10)
@@ -246,6 +259,6 @@ func TestHashMap_Vals(t *testing.T) {
 
 	vals := hm.Vals()
 
-	testsuite.AssertEqual(t, 10, len(vals))
-	testsuite.ContainsElements(t, elements, vals)
+	assert.Len(vals, 10)
+	assert.ElementsMatch(elements, vals)
 }
