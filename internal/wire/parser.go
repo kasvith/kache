7480ccb44161e1957a04a4bd3b19552cd5574d09
Kasun Vithanage <alankasun@gmail.com>
2019-03-20 10:25:19 +0530

    commented fixed errors by go vet

--- internal/wire/parser.go
+++ internal/wire/parser.go
@@ -7,14 +7,17 @@ import (
 	"github.com/kasvith/kache/internal/protocol"
 )
 
+// Parser is used to parse wire protocol
 type Parser struct {
 	r *bufio.Reader
 }
 
+// NewParser creates a wire protocol parser
 func NewParser(r *bufio.Reader) *Parser {
 	return &Parser{r: r}
 }
 
+// Parse and return a Command and an error
 func (p Parser) Parse() (*protocol.Command, error) {
 	str, err := p.r.ReadString('\n')
 	if err != nil {
