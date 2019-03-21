4c683cccc4278497af309eca8c88acd44fda317f
Kasun Vithanage <alankasun@gmail.com>
2019-03-21 10:17:54 +0530

    used initializer

--- internal/srv/tcp.go
+++ internal/srv/tcp.go
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c)  2018 Kasun Vithanage
+ * Copyright (c) 2019 Kasun Vithanage
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
  */
 
 package srv
@@ -54,7 +55,7 @@ func Start(config config.AppConfig) {
 			continue // we skip malformed user
 		}
 
-		client := &Client{Connection: conn}
+		client := NewClient(conn)
 		ConnectedClients.Add(client)
 		ConnectedClients.LogClientCount()
 
